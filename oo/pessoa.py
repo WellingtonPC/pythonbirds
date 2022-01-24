b881a0127ed7d8cdd9bed5ba3052512055ebab92
Wellington Pereira da Costa <well_pcosta@hotmail.com>
2022-01-23 21:32:47 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,8 @@
 class Pessoa:
+    #Atributos de classe são alocados apenas uma vez na memória
+    olhos = 2
 
+    #Atributos de instância são alocados sempre que um objeto é criado
     def __init__(self, *filhos:list, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
@@ -21,6 +24,12 @@ if __name__ == '__main__':
 
     lucioano.sobrenome = 'Ramalho'
     del lucioano.filhos
+    lucioano.olhos = 1
+    del lucioano.olhos
     print(lucioano.__dict__)
     print(joaquim.__dict__)
-    
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(lucioano.olhos)
+    print(joaquim.olhos)
+    print(id(Pessoa.olhos), id(lucioano.olhos), id(joaquim.olhos))
